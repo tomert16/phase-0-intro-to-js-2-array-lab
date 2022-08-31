b791465184fae446e2aa8ee335837657f2366f4b
tomer <tomertal52@yahoo.com>
2022-08-31 12:32:11 -0400

    commit tree eada229b53d2dbd810413d6565c441c1988e648d

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    name = "Ralph";
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    name = "Bob";
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    name = "Broom";
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    name = "Arnold";
+    const newCatsList = ["Arnold", ...cats];
+    return newCatsList;
+}
+
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
